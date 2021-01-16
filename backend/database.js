593d0677ca19ac5b1777ed09d4e5d0960f451c1e
Jeet Singh <taranjeet0007@gmail.com>
2021-01-17 01:11:02 +0700

    replaced key

--- backend/database.js
+++ backend/database.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 require('dotenv/config');
-const uri = process.env.DB_CONECTION;
+const uri = 'mongodb+srv://Jeet:Jeet0007@cluster0.kzud1.mongodb.net/StoneCold?retryWrites=true&w=majority';
 const params = { useNewUrlParser: true, useUnifiedTopology: true }
 const connectDb = async () => {
     const db = await mongoose.connect(uri, params);
